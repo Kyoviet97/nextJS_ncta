588d918eaea6ddc5ca05bd8a0ad5f54fab766694
kyoviet97 <kyoviet97@gmail.com>
2021-08-03 06:34:02 +0700

    change icon web

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,6 @@ function MyApp({ Component, pageProps }) {
       <div className={styles.main}>
         <Head>
           <meta name="theme-color" content="#0084ff" />
-          <link rel="icon" type="ImageType" href="https://api.nhaccutrangan.com/image/piano.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <script async defer src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v11.0&appId=729664794121020&autoLogAppEvents=1" nonce="GVdrlrv5"></script>
           <div id="fb-root"></div>
