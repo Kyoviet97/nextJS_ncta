9fdefe88a94f1a96c12fadfc81396fa93adf5e2e
DESKTOP-AAAHLSK\kyoviet97 <ducluong1203@gmail.com>
2022-06-26 23:14:52 +0700

    test 23

--- component/Header.js
+++ component/Header.js
@@ -9,7 +9,7 @@ import { useRouter } from 'next/router'
 // import { useRouter } from 'next/router'
 // import { useRouter } from 'next/router'
 // import { useRouter } from 'next/router'
-
+// import { useRouter } from 'next/router'
 
 const Header = () => {
     const router = useRouter()
