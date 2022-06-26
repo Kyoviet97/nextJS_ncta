7c691aea95daa82c3a4f5b00c515b1de5ab40e16
DESKTOP-AAAHLSK\kyoviet97 <ducluong1203@gmail.com>
2022-06-26 22:39:21 +0700

    Luong test

--- component/Header.js
+++ component/Header.js
@@ -5,6 +5,7 @@ import profilePic from '../public/images/logo.png'
 import menuIcon from '../public/images/menu.svg'
 import searchIcon from '../public/images/icon_search.svg'
 import cartIcon from '../public/images/icon_cart.svg'
+// import cartIcon from '../public/images/icon_cart.svg'
 import { useRouter } from 'next/router'
 
 const Header = () => {
